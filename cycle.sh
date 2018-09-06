5e5ab0a4a6898580b4b00c2d8d18dd4af11237dd
John Stockbauer <john@shippable.com>
2018-09-05 22:23:13 -0700

    finished cycle script

--- cycle.sh
+++ cycle.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #DO_TOKEN should be pre-defined
-export dropletId=""#required
-export snapshotId=""#computed later
-export snapshotName="factorio-ss"
 
 write_msg() {
   echo "---> $@"
@@ -12,11 +9,11 @@ exec_doctl() {
 }
 
 validate_prereqs() {
-  if [ -z "$dropletId" ]; then
-    echo "missing ID. cannot destroy."
+  if [ -z "$dropletId" && -z "$snapshotId" ]; then
+    echo "need a dropletId or a snapshotId to proceed"
     exit 99
   elif [ -z "$DO_TOKEN" ]; then
-    echo "missing DO token. cannot destroy."
+    echo "missing DO token. cannot use API."
     exit 99
   fi
   write_msg "all args present"
@@ -48,6 +45,12 @@ take_snapshot() {
   write_msg "done"
   write_msg "snapshot ID is: $snapshotId"
 }
+destroy_snapshot() {
+  write_msg "removing snapshot"
+  exec_doctl compute snapshot delete $snapshotId -f --no-header 
+  write_msg "done"
+  snapshotId=""
+}
 destroy_droplet() {
   write_msg "destroying droplet $dropletId"
   exec_doctl compute droplet delete $dropletId -f
@@ -55,17 +58,37 @@ destroy_droplet() {
 }
 restore_snapshot() {
   write_msg "creating new droplet from snapshot $snapshotId"
-  exec_doctl compute droplet create factorio --image $snapshotId --tag-name factorio --region sfo2 --size s-1vcpu-3gb --wait
+  exec_doctl compute droplet create factorio --image $snapshotId --tag-name factorio --region $region --size $size --wait
   write_msg "done"
   write_msg "finding new dropletId"
-  up_id=$(exec_doctl compute droplet list --format ID --no-header --tag-name factorio)
+  dropletId=$(exec_doctl compute droplet list --format ID --no-header --tag-name factorio)
   write_msg "done"
-  write_msg "found new id: $up_id"
+  write_msg "found new droplet id: $dropletId"
+}
+write_state() {
+  echo "snapshotId=\"$snapshotId\"" > state.env
+  echo "dropletId=\"$dropletId\"" >> state.env
+  shipctl copy_file_to_state /tmp/state.env
 }
 
+
 validate_prereqs
-get_droplet
-power_down
-take_snapshot
-destroy_droplet
-restore_snapshot
+if [ -n $dropletId && -z $snapshotId ]; then
+  get_droplet
+  power_down
+  sleep 2
+  take_snapshot
+  sleep 2
+  destroy_droplet
+  sleep 2
+  write_state
+elif [ -n $snapshotId && -z $dropletId ]; then 
+  restore_snapshot
+  sleep 2
+  destroy_snapshot
+  sleep 2
+  write_state
+fi
+
+write_msg "script complete"
+cat state.env
